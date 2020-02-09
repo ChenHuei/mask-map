42fedef3306dce7b75be202abc51db51438bd0e0
Leo Chen <leo.chen@cloud-interactive.com>
2020-02-09 18:03:17 +0800

    add Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loading-mask">
+    <div class="loading-icon"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/styles/imports';
+
+.loading {
+  &-mask {
+    @include size(100vw, 100vh);
+    @include flexCenter;
+    @include fixed;
+    background-color: color(mask);
+    z-index: 1000;
+  }
+
+  &-icon {
+    @include size(120px);
+    @include backgroundImage('../assets/Loading.svg');
+    position: relative;
+
+    &::after {
+      content: 'Loading';
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      color: color(white);
+      transform: translateX(-50%);
+    }
+  }
+}
+</style>
