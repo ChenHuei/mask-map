eda21208edc70fc9d20ca59792e2e1b4cd453900
Leo Chen <leo.chen@cloud-interactive.com>
2020-02-09 18:03:55 +0800

    add constants

---
+++ src/constants/api.js
@@ -0,0 +1,2 @@
+export const GET_DATA_API =
+  'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json?fbclid=IwAR1D-DFq2-DjeIRuVpKGwC-wLrunmZMHbvMY38JEexj8LEouf9vca-Wd8OU'
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const MASK_TYPE = [
+  { label: '全部', value: 'mask' },
+  { label: '成人', value: 'mask_adult' },
+  { label: '兒童', value: 'mask_child' }
+]
